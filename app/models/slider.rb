526b787afc209c6103eeb5c350a7bcacd3f4382b
Christopher Pezza <pezza.chris@gmail.com>
2017-04-09 10:27:04 -0400

    Update Slider Admin

--- app/models/slider.rb
+++ app/models/slider.rb
@@ -5,8 +5,5 @@ class Slider < ApplicationRecord
     content_type: ["image/jpeg", "image/gif", "image/png", "image/jpg"]
   }
   validates :order, presence: true
-  validates_inclusion_of :position,
-                         in: %w('' center-align left-alight right-align),
-                         message: "Poistion %{value} is not an option"
   scope :visable, -> { where(active: true).order(:order) }
 end
