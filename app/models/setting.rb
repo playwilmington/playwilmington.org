64d280cd78cbd93ae688a87fe2da753dee22ab1b
Christopher Pezza <pezza.chris@gmail.com>
2017-04-16 11:12:37 -0400

    Fix Settings callouts

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -12,26 +12,32 @@ class Setting < ApplicationRecord
   end
 
   def tel_url
+    return unless phone
     "tel:" + phone
   end
 
   def email_url
+    return unless email
     "mailto:" + email
   end
 
   def facebook_url
+    return unless facebook_handle
     "https://www.facebook.com/" + facebook_handle
   end
 
   def twitter_url
+    return unless twitter_handle
     "https://twitter.com/" + twitter_handle
   end
 
   def pinterest_url
+    return unless pinterest_handle
     "https://www.pinterest.com/" + pinterest_handle
   end
 
   def instagram_url
+    return unless instagram_handle
     "https://instagram.com/" + instagram_handle
   end
 end
