4eb1482fd7bb9b2e2e74b38ef7a485e99681efe6
Christopher Pezza <pezza.chris@gmail.com>
2017-04-22 17:07:16 -0400

    #17 Remove Foreign Key For Collections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420012853) do
+ActiveRecord::Schema.define(version: 20170422210447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -250,5 +250,4 @@ ActiveRecord::Schema.define(version: 20170420012853) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
-  add_foreign_key "collections", "pages"
 end
